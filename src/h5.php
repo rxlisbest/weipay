2f3f1c2cd3e68382b8556e96af1c74f5723a2b9c
Roy <rxlisbest@gmail.com>
2018-06-01 14:30:24 +0800

    项目初始化

---
+++ src/h5.php
@@ -0,0 +1,15 @@
+<?php
+namespace rxlisbest\weipay;
+/**
+ * Created by PhpStorm.
+ * User: ruixinglong
+ * Date: 2018/6/1
+ * Time: 下午2:19
+ */
+
+class h5
+{
+    public function test(){
+        var_dump(2);exit;
+    }
+}
